c4d508907d5436277967c83f1d84c8c11590eaf2
Atacturker <oturkeratac@gmail.com>
2024-10-15 18:08:15 +0300

    Update bankamatik.py

--- bankamatik.py
+++ bankamatik.py
@@ -1,6 +1,8 @@
+!pip install streamlit
 import sys
 import streamlit as st
 st.title("Bankamatik")
+st.write("Merhaba, bu benim ilk Streamlit uygulamam!")
 mail_list=[
     {"isim":"Turker", "yas":"25", "yer":"ankara", "mail":"turker@hotmail.com", "sifre":"7253", "bakiye": 23500},
     {"isim":"Necip", "yas":"32", "yer":"istanbul", "mail":"necip@hotmail.com", "sifre":"4327", "bakiye": 188000},
@@ -14,42 +16,42 @@ deneme=3
 while True: 
     for kullanıcı in mail_list:
         if mail==kullanıcı["mail"] and sifre==kullanıcı["sifre"]:
-            print("\nHoş geldiniz {} ".format(kullanıcı["isim"]))
+            st.write("\nHoş geldiniz {} ".format(kullanıcı["isim"]))
             y=1
             break
     if y==1:
         break
    
     deneme-=1
-    print(f"{deneme} deneme hakkınız kalmıştır.")
-    print("Lütfen tekrar deneyiniz. ")
+    st.write(f"{deneme} deneme hakkınız kalmıştır.")
+    st.write("Lütfen tekrar deneyiniz. ")
     mail=input("Lütfen mail adresinizi giriniz: ")
     sifre=input("Lütfen şifrenizi giriniz: ")
     if deneme==1:
-        print("Başarısız giriş yaptınız.")
+        st.write("Başarısız giriş yaptınız.")
         sys.exit()
     
  #%%           
 islem=int(input("\nLütfen yapmak istediğiniz işlem numarasını giriniz. \n1-Para Çekme\n2-Para Yatırma\nNo:"))
 if islem==1:
-    print("\nBakiyeniz {} Türk Lirasıdır.".format(kullanıcı["bakiye"]))
+    st.write("\nBakiyeniz {} Türk Lirasıdır.".format(kullanıcı["bakiye"]))
     cekme=int(input("\nLütfen çekmek istediğiniz nakit tutarı giriniz: "))
     while cekme>kullanıcı["bakiye"]:
-        print("\nLütfen çekmek istediğiniz nakit miktarı, bakiyenizi geçmesin.")
+        st.write("\nLütfen çekmek istediğiniz nakit miktarı, bakiyenizi geçmesin.")
         cekme=int(input("\nLütfen çekmek istediğiniz nakit tutarı giriniz: "))
-    print(f"\n{cekme} Türk lirasını alabilirsiniz.")
+    st.write(f"\n{cekme} Türk lirasını alabilirsiniz.")
     kullanıcı["bakiye"]=kullanıcı["bakiye"]-cekme
-    print("\nKalan bakiyeniz: {} Türk lirasıdır".format(kullanıcı["bakiye"]))
-    print("\nİyi günler dileriz.")
+    st.write("\nKalan bakiyeniz: {} Türk lirasıdır".format(kullanıcı["bakiye"]))
+    st.write("\nİyi günler dileriz.")
     
 elif islem==2:
-    print("\nBakiyeniz {} Türk Lirasıdır.".format(kullanıcı["bakiye"]))
+    st.write("\nBakiyeniz {} Türk Lirasıdır.".format(kullanıcı["bakiye"]))
     
     yatırma=int(input("\nLütfen yatırmak istediğiniz nakit tutarı giriniz: "))
     
-    print(f"\n{yatırma} Türk lirasını lütfen hazneye yerleştiriniz.")
+    st.write(f"\n{yatırma} Türk lirasını lütfen hazneye yerleştiriniz.")
     
     kullanıcı["bakiye"]=kullanıcı["bakiye"]+yatırma
-    print("\nBakiyeniz: {} Türk lirası olacak şekilde güncellenmiştir".format(kullanıcı["bakiye"]))
+    st.write("\nBakiyeniz: {} Türk lirası olacak şekilde güncellenmiştir".format(kullanıcı["bakiye"]))
     
-    print("\nİyi günler dileriz.") 
+    st.write("\nİyi günler dileriz.") 
